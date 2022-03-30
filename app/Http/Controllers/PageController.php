e394cd8bfd4ece59eb1a26003ff96f6581441edc
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-30 19:20:35 +0200

    Esercizio completato con qualche query di prova

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,6 +11,13 @@ class PageController extends Controller
         //recupero tutti i film dal database
         $movies = Movie::all();
 
+        //recupero tutti i film ordinati per voto
+        //$movies = Movie::orderBy('vote', 'DESC')->get();
+
+        //recupero tutti i film con un voto superiore o uguale a 8 
+        //ordinati per voto
+        //$movies = Movie::where('vote', '>=',8)->orderBy('vote', 'DESC')->get();
+
         //li passo alla vista "Home"
         return view('home', compact('movies'));
     }
